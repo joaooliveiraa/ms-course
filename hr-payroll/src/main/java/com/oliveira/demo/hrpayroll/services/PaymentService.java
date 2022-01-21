a4c6b1aead83e914d9fc85020bc8a9be46c76f2d
tomanocu69G <joaogetknow@gmail.com>
2022-01-21 10:51:27 -0300

    Feign Client

--- hr-payroll/src/main/java/com/oliveira/demo/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/oliveira/demo/hrpayroll/services/PaymentService.java
@@ -1,31 +1,22 @@
 package com.oliveira.demo.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.oliveira.demo.hrpayroll.entities.Payment;
 import com.oliveira.demo.hrpayroll.entities.Worker;
+import com.oliveira.demo.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}") //acessar valor da variavel do aplication properties
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker =  workerFeignClient.findById(workerId).getBody(); //Pegar na resposta que é tipo worker
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
