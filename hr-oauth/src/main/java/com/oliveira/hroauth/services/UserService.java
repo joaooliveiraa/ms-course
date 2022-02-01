341f8ca08fd132982dfa990a4a6cc08be28bc61d
tomanocu69G <joaogetknow@gmail.com>
2022-02-01 17:06:02 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/oliveira/hroauth/services/UserService.java
@@ -0,0 +1,24 @@
+package com.oliveira.hroauth.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oliveira.hroauth.entities.User;
+import com.oliveira.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		
+		User user = userFeignClient.findByEmail(email).getBody();
+		if( user == null) {
+			throw new IllegalArgumentException("Email not found");
+		}
+		return user;
+	}
+
+}
